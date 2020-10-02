ae5ffc95e42137b2cd3e6b695c4227d45a92be36
Leandro Lima <leandro.lima@oowlish.com>
2020-10-01 23:45:04 -0300

    adding basic database config

---
+++ config/database.js
@@ -0,0 +1,25 @@
+const typeorm = require('typeorm');
+const {
+  DB_HOST='localhost',
+  DB_PORT=5432,
+  DB_USERNAME='postgres',
+  DB_PASSWORD='root',
+  DB_NAME='postgres',
+} = process.env;
+
+
+function connectToDatabase() {
+  return typeorm.createConnection({
+    type: 'postgres',
+    host: DB_HOST,
+    port: DB_PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [],
+  });
+}
+
+module.exports = connectToDatabase;
--- config/server.js
+++ config/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const connectToDatabase = require('./database');
 const app = express();
+
+const { SERVER_PORT = 3000 } = process.env;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-module.exports = app;
+async function initializeServer() {
+  await connectToDatabase();
+  app.listen(SERVER_PORT, () => console.log(`Server is listening on port: ${SERVER_PORT}`));
+}
+
+module.exports = initializeServer;
