e92ec159382141aed4ad7aaec77b5b3c08cb5c2e
Raul Aquino <aquinoraul207@gmail.com>
2020-10-31 10:29:37 -0300

    add controllers but but we still need to add the model

---
+++ src/controllers/BagController.js
@@ -0,0 +1,7 @@
+const axios = require('axios');
+
+module.exports = {
+    async show(req, res) { 
+        
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const BagController = require('./controllers/BagController');
 
 const routes = express.Router();
 
+routes.get('/bags', BagController.show);
 
 module.exports = routes;
